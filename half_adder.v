85ebaf74b25e3b672db7e2d4a70a5ff5719ef0e4
N2veen8 <154792924+N2veen8@users.noreply.github.com>
2025-02-20 00:52:33 +0530

    Update half_adder.v

--- half_adder.v
+++ half_adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.02.2025 17:16:47
-// Design Name: 
-// Module Name: half_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module half_adder(a,b,sum,carry);
   input a,b;
