67671e25f1fbdf19a4a83a62537c4100a100772b
N2veen8 <154792924+N2veen8@users.noreply.github.com>
2025-02-20 00:52:58 +0530

    Update hlaf_adder_tb.v

--- hlaf_adder_tb.v
+++ hlaf_adder_tb.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.02.2025 17:21:22
-// Design Name: 
-// Module Name: hlaf_adder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 
